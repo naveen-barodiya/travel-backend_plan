adab6075370cb1b663075b7da8ccde20dea76d40
naveen kumar <nk143barodiya@gmail.com>
2026-02-07 13:01:40 +0530

    changed..........

--- config/openai.js
+++ config/openai.js
@@ -1,6 +1,11 @@
 import OpenAI from "openai";
 
-const OPENAI_API_KEY =" process.env.OPENAI_API_KEY";
+import dotenv from "dotenv";
+dotenv.config();
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
+
+console.log("🔑 OpenAI Key Loaded:", process.env.OPENAI_API_KEY);
 
 if (!OPENAI_API_KEY) {
     throw new Error("❌ OpenAI API Key missing");
