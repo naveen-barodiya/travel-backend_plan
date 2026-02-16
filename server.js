327180c87dcfeb727968b90b18978958fd06eda1
naveen kumar <nk143barodiya@gmail.com>
2026-02-16 12:04:16 +0530

    then complete error fixed again  now.....

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import travelRoutes from "./routes/travelRoutes.js";
 
 dotenv.config();
 connectDB();
