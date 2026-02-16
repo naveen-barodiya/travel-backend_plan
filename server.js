b1d86625550e2a7ba77077c311d0f8354df372a0
naveen kumar <nk143barodiya@gmail.com>
2026-02-16 09:59:50 +0530

    all updated......

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ connectDB();
 
 
 
-app.use(cors());
+
+
+app.use(
+    cors({
+        origin: "https://travel-frontend-plan.vercel.app",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        credentials: true,
+    })
+);
+
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
